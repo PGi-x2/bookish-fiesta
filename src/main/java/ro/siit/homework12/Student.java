655c76fc82dbf51899cc4c41233df8f855591958
Porgras Andrei <85518940+PGi-x2@users.noreply.github.com>
2021-08-08 21:53:23 +0300

    update

---
+++ src/main/java/ro/siit/homework12/Student.java
@@ -0,0 +1,9 @@
+package ro.siit.homework12;
+
+public class Student implements ID {
+    public String firstName = "Knight";
+    public String lastName = "Phil";
+    public String dateOfBirth = "24.02.1938";
+    public String gender = "M";
+    public String ID = "1380224389723";
+}
--- src/main/java/ro/siit/homework5/ID.java
+++
@@ -1,4 +0,0 @@
-package ro.siit.homework5;
-
-public interface ID {
-}
