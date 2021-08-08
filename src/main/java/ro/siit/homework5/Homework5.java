cfa5a0096ad70edc3e86b90f352d872744fd24e0
Porgras Andrei <85518940+PGi-x2@users.noreply.github.com>
2021-08-08 21:42:31 +0300

    update

--- src/main/java/ro/siit/homework5/Homework5.java
+++
@@ -1,9 +0,0 @@
-package ro.siit.homework5;
-
-public class Homework5 {
-    public static void main(String[] args) {
-        System.out.println("that's cool");
-        int anger = 99;
-        System.out.println(anger);
-    }
-}
---
+++ src/main/java/ro/siit/homework5/Student.java
@@ -0,0 +1,14 @@
+package ro.siit.homework5;
+
+public class Student implements ID{
+    public String firstName = "Knight";
+    public String lastName = "Phil";
+    public String dateOfBirth = "24.02.1938";
+    public String gender = "M";
+    public String ID = "1380224389723";
+
+    @Override
+    public String getCNP() {
+        return null;
+    }
+}
